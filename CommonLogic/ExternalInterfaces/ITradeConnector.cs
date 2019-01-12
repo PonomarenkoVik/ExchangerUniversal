29170f8b24c40dff239628144a327889ded59c4f
dev <pvikmboxhome@gmail.com>
2019-01-12 20:18:29 +0200

    Commit3

---
+++ CommonLogic/ExternalInterfaces/ITradeConnector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLogic.ExternalInterfaces
+{
+    public interface ITradeConnector
+    {
+        
+    }
+}
